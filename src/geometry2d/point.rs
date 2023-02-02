2df72c5a697f174cd60126e47d5702ffd0a0625a
Grovre <50428844+Grovre@users.noreply.github.com>
2023-02-01 18:16:32 -0600

    distance between points

--- src/geometry2d/point.rs
+++ src/geometry2d/point.rs
@@ -4,5 +4,9 @@ pub struct Point {
 }
 
 impl Point {
-
+    pub fn distance(&self, other: &Self) -> f32 {
+        let xx = (self.X - other.X) * (self.X - other.X);
+        let yy = (self.Y - other.Y) * (self.Y - other.Y);
+        (xx + yy).sqrt()
+    }
 }
